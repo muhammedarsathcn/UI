4a723ce08dcc82a2d88ebda6fdb2282e5bb9c248
muhammedarsathcn <muhammed.arsathcn@cdw.com>
2026-03-27 14:39:27 +0530

    Intentation fixed

--- js-basics/section2.js
+++ js-basics/section2.js
@@ -13,17 +13,22 @@ const cricketPlayers = [
   "Mohammed Siraj",
 ];
 console.log("List of Cricket Players:", cricketPlayers);
+
 // 2. Unfortunately, the first player had an injury. Remove him from the list of players.
 cricketPlayers.shift();
 console.log("After removing the first player", cricketPlayers);
+
 // 3. Now, find out the number of players
 console.log("Number Of players:", cricketPlayers.length);
+
 //4. Add another player to the above list of players to make the count 11.
 cricketPlayers.unshift("Ms Dhoni");
 console.log("After adding new Player 1", cricketPlayers);
+
 // 5. The cricket board has decided to take photographs of all players and so they would need the players list in sorted format.
 cricketPlayers.sort();
 console.log("Arranged players for photograph:", cricketPlayers);
+
 // 6. Display all the Players name and assign a random jersey number. For example. MS Dhoni-7
 let jerseyNumbers = [];
 console.log("Players with their jersey number");
@@ -38,6 +43,7 @@ cricketPlayers.forEach((player) => {
     }
   }
 });
+
 // 7. The cricket board wants to print the names of all players in uppercase and store it in a different location for printing jerseys. Do not modify the existing players list.
 const uppercasePlayers = cricketPlayers.map((player) => {
   return player.toUpperCase();
