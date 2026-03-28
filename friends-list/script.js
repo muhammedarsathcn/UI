105507883526fbcdc3a0f0218e90a8bad6f8f00d
muhammedarsathcn <muhammed.arsathcn@cdw.com>
2026-03-28 12:28:06 +0530

    Designing completed

---
+++ friends-list/script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+//fetching data from the friends.json
+fetch("./data/friends.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((friend) => {
+      const card = document.createElement("article");
+      card.classList.add("card");
+      card.innerHTML = `
+        <figure class="profile">
+        <img src="${friend.img} alt="${friend.first_name} />
+        </figure>
+        <section>
+        <p class="name">${friend.first_name} ${friend.last_name}</p>
+        <p class="email">${friend.email}</p>
+        </section>
+        `;
+      wrapper.appendChild(card);
+    });
+  })
+  .catch((e) => {
+    console.error("Error in fetching friends-list", e);
+  });
