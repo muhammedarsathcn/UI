9a4fbcf76357c59794c5b545c6e8ef68250d4bb3
muhammedarsathcn <muhammed.arsathcn@cdw.com>
2026-03-31 15:34:48 +0530

    review comments changed

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,51 +1,65 @@
 const form = document.querySelector(".form");
-// validators for each input fields
+// validators and error messages for each input fields
 const validators = {
-  firstName: (value) => /^[A-Za-z]{1,30}$/.test(value),
-  lastName: (value) => /^[A-Za-z]{1,30}$/.test(value),
-  email: (value) => /^[A-Za-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,}/i.test(value),
-  contactNumber: (value) => /^[6-9]\d{9}$/.test(value),
-  pinCode: (value) => /^\d{6}$/.test(value),
-  cardNumber: (value) => /^\d{16}$/.test(value),
-  cardExpiry: (value) => {
-    const currentYear = new Date().getFullYear();
-    return /^\d{4}$/.test(value) && Number(value) >= currentYear;
-  },
-  cvv: (value) => /^\d{3,4}$/.test(value),
-};
-//messages
-const message = {
   firstName: {
-    empty: "First Name is required",
-    invalid: "First Name is not valid",
+    validate: (value) => /^[A-Za-z]{1,30}$/.test(value),
+    message: {
+      empty: "First Name is required",
+      invalid: "First Name is not valid",
+    },
   },
   lastName: {
-    empty: "Last Name is required",
-    invalid: "Last Name is not valid",
+    validate: (value) => /^[A-Za-z]{1,30}$/.test(value),
+    message: {
+      empty: "Last Name is required",
+      invalid: "Last Name is not valid",
+    },
   },
   email: {
-    empty: "Email Address is required",
-    invalid: "Email Address is not valid",
+    validate: (value) =>
+      /^[A-Za-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,}/i.test(value),
+    message: {
+      empty: "Email Address is required",
+      invalid: "Email Address is not valid",
+    },
   },
   contactNumber: {
-    empty: "Contact Number is required",
-    invalid: "Contact Number is not valid",
+    validate: (value) => /^[6-9]\d{9}$/.test(value),
+    message: {
+      empty: "Contact Number is required",
+      invalid: "Contact Number is not valid",
+    },
   },
   pinCode: {
-    empty: "PIN Code is required",
-    invalid: "PIN Code is not valid",
+    validate: (value) => /^\d{6}$/.test(value),
+    message: {
+      empty: "PIN Code is required",
+      invalid: "PIN Code is not valid",
+    },
   },
   cardNumber: {
-    empty: "Card Number is required",
-    invalid: "Card Number is not valid",
+    validate: (value) => /^\d{16}$/.test(value),
+    message: {
+      empty: "Card Number is required",
+      invalid: "Card Number is not valid",
+    },
   },
   cardExpiry: {
-    empty: "Card Expiry is required",
-    invalid: "Card Expiry is not valid",
+    validate: (value) => {
+      const currentYear = new Date().getFullYear();
+      return /^\d{4}$/.test(value) && Number(value) >= currentYear;
+    },
+    message: {
+      empty: "Card Expiry is required",
+      invalid: "Card Expiry is not valid",
+    },
   },
   cvv: {
-    empty: "CVV is required",
-    invalid: "CVV is not valid",
+    validate: (value) => /^\d{3,4}$/.test(value),
+    message: {
+      empty: "CVV is required",
+      invalid: "CVV is not valid",
+    },
   },
 };
 // event listener for the form
@@ -62,11 +76,11 @@ function validateInputField(input) {
   const name = input.name;
   const errorText = input.nextElementSibling;
   if (value === "") {
-    showError(input, errorText, message[name].empty);
+    showError(input, errorText, validators[name].message.empty);
     return;
   }
-  if (!validators[name](value)) {
-    showError(input, errorText, message[name].invalid);
+  if (!validators[name].validate(value)) {
+    showError(input, errorText, validators[name].message.invalid);
     return;
   }
   clearErrorMsg(input, errorText);
@@ -81,9 +95,3 @@ function clearErrorMsg(input, error) {
   error.textContent = "";
   input.classList.remove("error-border");
 }
-//dynamic input validation
-document.querySelectorAll(".input-field").forEach((input) => {
-  input.addEventListener("input", function () {
-    validateInputField(input);
-  });
-});
