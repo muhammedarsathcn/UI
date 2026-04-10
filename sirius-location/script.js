9b347c2286beb2ea5a1c70378dfab729a33ad539
muhammedarsathcn <muhammed.arsathcn@cdw.com>
2026-04-10 09:07:17 +0530

    review comments changed

--- sirius-location/script.js
+++ sirius-location/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //Accordion mapping
   $("#accordion").accordion({
     collapsible: true,
     heightStyle: "content",
@@ -9,52 +10,57 @@ $(document).ready(function () {
       activeHeader: "ui-icon-triangle-1-s",
     },
   });
-});
-const countryCodes = {
-  "United States": "US",
-  India: "IN",
-  Canada: "CA",
-  "United Kingdom": "GB",
-};
-$(document).ready(function () {
+
+  // Country codes mapping
+  const countryCodes = {
+    "United States": "US",
+    India: "IN",
+    Canada: "CA",
+    "United Kingdom": "GB",
+  };
+
+  // Fetch locations data
+  // Fetch locations data
   $.getJSON("./data/locations.json", function (locations) {
-    let locationHTML = "";
     locations.forEach(function (location) {
-      locationHTML = `
-        <article class="location-card">
-          <span class="country">
-            <figure class="flag">
-              <img src="https://flagsapi.com/${countryCodes[location.country]}/flat/64.png" alt="${location.country}" />
-            </figure>
-            <p>${location.state}</p>
-          </span>
-          <p class="city">${location.city}</p>
-          <p class="contact-number">${location.contact}</p>
-        </article>
-      `;
-      $("#section-three").append(locationHTML);
+      const locationDiv = $("<div>").addClass("location");
+
+      const flagWrapper = $("<span>").addClass("flag-wrapper");
+
+      const figure = $("<figure>").addClass("flag-container");
+
+      const flagImg = $("<img>")
+        .attr(
+          "src",
+          `https://flagsapi.com/${countryCodes[location.country]}/flat/64.png`,
+        )
+        .attr("alt", location.country);
+
+      const countryText = $("<p>").addClass("country").text(location.state);
+
+      const stateText = $("<p>").addClass("state").text(location.city);
+
+      const phoneText = $("<p>").addClass("phone-no").text(location.contact);
+
+      // Build structure
+      figure.append(flagImg);
+
+      flagWrapper.append(figure, countryText);
+
+      locationDiv.append(flagWrapper, stateText, phoneText);
+
+      $("#section-three").append(locationDiv);
     });
   });
-});
 
-$("#tabs").tabs();
-
-$(document).ready(function () {
+  // jQuery UI tabs
+  $("#tabs").tabs();
+  // Tab switching logic
   $(".tab").click(function () {
-    const tabText = $(this).text().trim();
-    $("#section-one").hide();
-    $(".section-two").hide();
-    $(".section-three").hide();
+    const target = $(this).data("target");
+    $(".section-one, .section-two, .section-three").hide();
     $(".tab").removeClass("active-tab");
-
     $(this).addClass("active-tab");
-
-    if (tabText === "ABOUT US") {
-      $(".section-one").show();
-    } else if (tabText === "SOLUTIONS") {
-      $(".section-two").show();
-    } else if (tabText === "LOCATIONS") {
-      $(".section-three").show();
-    }
+    $("." + target).show();
   });
 });
